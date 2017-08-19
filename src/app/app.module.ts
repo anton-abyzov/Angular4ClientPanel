e9d686b63d9ff6d787ab47e0edc1a5f881bdf8ad
Anton <anton.abyzov@gmail.com>
2017-08-19 03:56:57 +0300

    basics + login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
+//AngularFire imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database'
+import { AngularFireAuth } from 'angularfire2/auth'
+
+//component imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+//service imports
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent},
+  {path:'add-client', component:AddClientComponent},
+  {path:'client/:id', component:ClientDetailsComponent},
+  {path:'edit-client/:id', component:EditClientComponent}
+  // {path:'register', component:RegisterComponent},
+  // {path:'register', component:RegisterComponent},
+  // {path:'register', component:RegisterComponent},
+]
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC60aVrcl3Qq4N0kF0R1Mh0EQkQW5ILFZI",
+  authDomain: "clientpanel-1cccf.firebaseapp.com",
+  databaseURL: "https://clientpanel-1cccf.firebaseio.com",
+  storageBucket: "clientpanel-1cccf.appspot.com",
+  messagingSenderId: "655825684533"
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [AngularFireAuth, AngularFireDatabase, ClientService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  isLoggedIn:boolean;
+  loggedInUser:string;
+  showRegister:boolean;
+
+  constructor(
+    public router:Router, public authService:AuthService, public flashMessagesService:FlashMessagesService
+  ) { }
+
+  ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth){
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      }
+      else{
+        this.isLoggedIn = false;
+      }
+    })
+  }
+
+  onLogoutClick(){
+    this.authService.logout();
+    this.flashMessagesService.show('You are logged out', {cssClass:'alert-success', timeout:4000});
+    this.router.navigate(['/login']);
+  }
+
+}
