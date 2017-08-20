968e175da272688d57e2a5bf99429d9d826b63b2
Anton <anton.abyzov@gmail.com>
2017-08-20 23:22:08 +0300

    settings complete

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../services/client.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Client } from '../../models/Client';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-edit-client',
@@ -22,7 +23,8 @@ export class EditClientComponent implements OnInit {
   disableBalanceOnEdit:boolean = true;
 
   constructor(
-    public clientService:ClientService, public router:Router, public route:ActivatedRoute, public flashMessagesService:FlashMessagesService
+    public clientService:ClientService, public router:Router, public route:ActivatedRoute, public flashMessagesService:FlashMessagesService,
+    public settingsService:SettingsService
   ) { }
 
   ngOnInit() {
@@ -31,6 +33,7 @@ export class EditClientComponent implements OnInit {
       this.client = client;
       console.log(client);
     });
+    this.disableBalanceOnEdit = this.settingsService.getSettings().disableBalanceOnEdit;
   }
 
   onSubmit({value, valid}:{value:Client, valid:boolean}){
